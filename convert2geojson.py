49742d967fb1ad28f1fd3b797e13622723f4c61e
Дмитрий <dimitrius-brest@users.noreply.github.com>
2015-08-31 21:45:31 +0300

    Update convert2geojson.py

--- convert2geojson.py
+++ convert2geojson.py
@@ -5,6 +5,12 @@ with open('input.txt', 'r', encoding="utf-8") as f:
 
 import re
 
-ssylka = "content:.*<a href=\"(.*)\">"
+ssylka = "content:.*<a href=\"(.*)\">"  # вычленяет ссылку
+koord = "position:.*\((.*),(.*)\)"      # вычленяет координаты
+nazv = "title:.*\"(.*)\""               # вычленяет название
+tippos = "icon:.*\/marker_(.*)\.png"    # вычленяет тип поселения
 
 print(re.findall(ssylka, text))
+print(re.findall(koord, text))
+print(re.findall(nazv, text))
+print(re.findall(tippos, text))
